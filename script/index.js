df12b6b5baca5b8a166bb3b65d5c0a4b9ec10441
Likhon-25 <likhonmondol327@gmail.com>
2026-03-06 02:18:10 +0600

    Responsive compleate

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ const createElements = (arr) => {
     return (htmlElemtnst.join(" "));
 }
 
+function pronounceWord(word) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "en-EN"; // English
+    window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) => {
     if (status == true) {
         document.getElementById("spinner").classList.remove("hidden")
@@ -78,7 +84,7 @@ const displayLevelWord = (words) => {
         wordContainer.innerHTML = `
             <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
             <img src="./assets/alert-error.png" alt="No found word" class="mx-auto">
-            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
             <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
          </div>
         `;
@@ -87,16 +93,16 @@ const displayLevelWord = (words) => {
     }
 
     words.forEach((word) => {
-        // console.log(word);
-        const card = document.createElement("div");
+        const card = document.createElement("div"); 
+        card.className = "h-full";                  
         card.innerHTML = `
-         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4 h-full flex flex-col justify-between">
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <p class="font-semibold">Meaning / Pronounciation</p>
             <div class="text-2xl font-medium font-bangla">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} /${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}</div>
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
         `
@@ -104,6 +110,7 @@ const displayLevelWord = (words) => {
     });
     manageSpinner(false);
 };
+
 
 const displayLesson = (lessons) => {
     //* 1. get the container
@@ -143,3 +150,4 @@ document.getElementById("btn-search").addEventListener("click", () => {
 
         });
 });
+
--- test.js
+++
@@ -1,8 +0,0 @@
-
-const createElements = (arr) =>{
-const htmlElemtnst = arr.map(el => `<span class="btn>${el}</span>`);
-console.log(htmlElemtnst.join(" "));
-}
-
-const synoyms=["hello", "hi", "Konnichiwa"];
-createElements(synoyms)
