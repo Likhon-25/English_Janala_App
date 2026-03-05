b5d273fdd0c3ba298d0008d136ca6e45c9f5265b
Likhon-25 <likhonmondol327@gmail.com>
2026-03-05 22:53:24 +0600

    Lesson Button Activation Compleate

--- script/index.js
+++ script/index.js
@@ -4,11 +4,21 @@ const loadLesson = () => {
         .then((json) => displayLesson(json.data))
 }
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-btn");
+    lessonButtons.forEach((btn) => btn.classList.remove("active"));
+}
+
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then((res) => res.json())
-        .then((data) => displayLevelWord(data.data))
+        .then((data) => {
+            removeActive(); // remove all active class
+            const clickBtn = document.getElementById(`lesson-btn-${id}`);
+            clickBtn.classList.add("active"); // add active class
+            displayLevelWord(data.data)
+        });
 }
 
 const displayLevelWord = (words) => {
@@ -53,7 +63,8 @@ const displayLesson = (lessons) => {
         //* 3.    create Element
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <button id="lesson-btn-${lesson.level_no}"
+        onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
         </button>
         `
